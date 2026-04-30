76707351224615fdde91a878e32a6f868e4d1a54
Tareq <tariqulislamtareq1670@gmail.com>
2026-04-30 15:07:53 +0600

    update authClient baseURL to correct deployment link

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "https://skill-sphere-iuclgnh7e-tareq1670s-projects.vercel.app"
+    baseURL: "https://skill-sphere-7eoq9f0zf-tareq1670s-projects.vercel.app"
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()
